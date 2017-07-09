76cc0289c61cc281b6f85d9c518fef44c3a40eca
Donovan Mulder <donovan.mulder@kineticskunk.com>
2017-07-09 19:20:03 +0200

    Some new mongoDB stuff

---
+++ src/main/java/com/kineticskunk/auto/mongodb/TestDataLoader.java
@@ -0,0 +1,31 @@
+package com.kineticskunk.auto.mongodb;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.kineticskunk.utilities.ConfigurationLoader;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+public class TestDataLoader extends ConfigurationLoader {
+	
+	private final Logger logger = LogManager.getLogger(TestDataLoader.class.getName());
+	private final Marker TESTDATALOADER = MarkerManager.getMarker("TESTDATALOADER");
+	
+	private JSONParser parser = new JSONParser();
+	private JSONObject jsonObject = null;
+	
+	public TestDataLoader(String jsonFile) {
+		super(jsonFile);
+	}
+	
+	
+	
+}
